ddfed6e091f9576755ac1ed378e03f4ab49b78c6
Sagnik Mitra <sagnikmitra123@gmail.com>
2025-02-02 16:36:11 +0530

    color fixed for bold text for old regime

--- calculator.py
+++ calculator.py
@@ -96,12 +96,18 @@ def set_branding():
                 color: white;
                 text-align: center;
             }
-            .highlight-old {
+            .highlight-pre-old {
+                font-size: 1.2rem;
+                font-weight: bold;
+                color: #ffcc00;
+            }
+            
+            .highlight-pre-new {
                 font-size: 1.2rem;
                 font-weight: bold;
                 color: #2563EB;
             }
-            
+
             .highlight-new {
                  font-size: 1.2rem;
                 font-weight: bold;
@@ -184,14 +190,14 @@ if salary > 0:
     st.markdown(f"<div class='result-box-pre-old'>{format_currency(pre_budget_old_regime_tax_post_cess)}</div>", unsafe_allow_html=True)
     st.warning("Shown tax includes 4 percent CESS")
     pre_budget_old_regime_tax_in_hand_per_month_salary = int((salary - pre_budget_old_regime_tax_post_cess)/12)
-    st.markdown(f"<p class='highlight-old'>Effective Rate: {pre_budget_old_regime_effective_rate:.1f}%<br>In-Hand Per Month Salary: {pre_budget_old_regime_tax_in_hand_per_month_salary}</p>", unsafe_allow_html=True)
+    st.markdown(f"<p class='highlight-pre-old'>Effective Rate: {pre_budget_old_regime_effective_rate:.1f}%<br>In-Hand Per Month Salary: {pre_budget_old_regime_tax_in_hand_per_month_salary}</p>", unsafe_allow_html=True)
     
     st.markdown("<h2 class='sub-title'>Pre-Budget New Regime Tax Calculation as per 2024</h2>", unsafe_allow_html=True)
     pre_budget_new_regime_tax_post_cess = pre_budget_new_regime_tax*1.04
     st.markdown(f"<div class='result-box-pre-new'>{format_currency(pre_budget_new_regime_tax_post_cess)}</div>", unsafe_allow_html=True)
     st.info("Shown tax includes 4 percent CESS")
     pre_budget_new_regime_tax_in_hand_per_month_salary = int((salary - pre_budget_new_regime_tax_post_cess)/12)
-    st.markdown(f"<p class='highlight-old'>Effective Rate: {pre_budget_new_regime_effective_rate:.1f}%<br>In-Hand Per Month Salary: {pre_budget_new_regime_tax_in_hand_per_month_salary}</p>", unsafe_allow_html=True)
+    st.markdown(f"<p class='highlight-pre-new'>Effective Rate: {pre_budget_new_regime_effective_rate:.1f}%<br>In-Hand Per Month Salary: {pre_budget_new_regime_tax_in_hand_per_month_salary}</p>", unsafe_allow_html=True)
     
     st.markdown("<h2 class='sub-title'>New Tax Calculation as per 2025</h2>", unsafe_allow_html=True)
     post_budget_new_tax_post_cess = post_budget_new_tax*1.04
